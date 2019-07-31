f74c48f90379a1737ac7ddbcbc86c16d887d1472
MansoorBashaBellary <mbellary@chewy.com>
2019-07-31 12:35:27 -0400

    feat(tabs): add element resize detector

--- src/tabs/src/tabs.js
+++ src/tabs/src/tabs.js
@@ -1,9 +1,10 @@
 import { tns } from 'tiny-slider/src/tiny-slider';
+import resizeDetector from 'element-resize-detector';
 
 export default class Tabs {
   // disable: eslint
   constructor(name) {
     this.name = name;
-    console.log('Tabs 1', tns);
+    console.log('Tabs 1', tns, resizeDetector);
   }
 }
