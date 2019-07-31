dd957a2dab21469bf3288e19edbf086a8109ccc5
MansoorBashaBellary <mbellary@chewy.com>
2019-07-31 18:44:47 -0400

    feat(tabs): removing build4

--- src/tabs/src/tabs.js
+++ src/tabs/src/tabs.js
@@ -1,9 +1,6 @@
-import { tns } from 'tiny-slider/src/tiny-slider';
-
 export default class Tabs {
   // disable: eslint
   constructor(name) {
     this.name = name;
-    console.log('Tabs 1', tns);
   }
 }
