a27277f35455e36173075ceda1f7c61da141ef42
MansoorBashaBellary <mbellary@chewy.com>
2019-07-31 18:39:27 -0400

    feat(tabs): removing build2

--- src/tabs/src/tabs.js
+++ src/tabs/src/tabs.js
@@ -1,10 +1,9 @@
 import { tns } from 'tiny-slider/src/tiny-slider';
-import resizeDetector from 'element-resize-detector';
 
 export default class Tabs {
   // disable: eslint
   constructor(name) {
     this.name = name;
-    console.log('Tabs 1', tns, resizeDetector);
+    console.log('Tabs 1', tns);
   }
 }
